2104c74fe9e8d0615cfd0bf2af33dc4645098084
Richard Bronkhorst <richmans@users.noreply.github.com>
2015-04-15 06:53:29 +0200

    moved setup to the root to make this an honest python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+diskco_module=Extension('diskco',
+    sources= ['diskco_python/diskco_python.cpp',
+   'diskco/buffer.cpp',
+   'diskco/buffer_processor.cpp',
+   'diskco/byte_swapper.cpp',
+   'diskco/diskco.cpp',
+   'diskco/file_reader.cpp',
+   'diskco/file_writer.cpp',
+   'diskco/options.cpp',
+   'diskco/progress.cpp',
+   'diskco/utils.cpp'
+  ],
+)
+
+setup (name = 'Diskco',
+  description = 'Fast dd functionality',
+  author = 'Richard',
+  ext_modules=[diskco_module]
+
+)
